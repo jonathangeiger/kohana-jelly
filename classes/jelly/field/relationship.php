b335a8f9a670537499431fad220e95288d7d682b
jonathangeiger <jonathan@jonathan-geiger.com>
2010-02-09 15:05:51 -0500

    Fixing class comments

--- classes/jelly/field/relationship.php
+++ classes/jelly/field/relationship.php
@@ -1,16 +1,11 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
 /**
- * Allows a field to declare that it can be joined using with() in the model
+ * Declares a relationship field, which has special characteristics in the mode.
  *
  * @package jelly
  */
 interface Jelly_Field_Relationship
 {	
-	/**
-	 * @param string $model 
-	 * @param string $ids 
-	 * @return boolean
-	 */
 	public function has($model, $ids);
 }
