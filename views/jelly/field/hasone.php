278d61063a7694681e098ea071289ebb0cd1a368
Jonathan Geiger <jonathan@jonathan-geiger.com>
2010-02-15 15:54:58 -0500

    Fixing a few issues pointed out by SpadXIII. Thanks!

--- views/jelly/field/hasone.php
+++ views/jelly/field/hasone.php
@@ -1,7 +1,7 @@
 <select name="<?php echo $name ?>" id="field-<?php echo $name ?>">
 	<?php foreach(Jelly::factory($foreign_model)->load() as $related): ?>
 		<?php if ($related->id() == $id): ?>
-			<option value="<?php echo $related->id() ?>" selected='selected'><?php echo $related->name()?></option>
+			<option value="<?php echo $related->id() ?>" selected="selected"><?php echo $related->name()?></option>
 		<?php else: ?>
 			<option value="<?php echo $related->id() ?>"><?php echo $related->name()?></option>
 		<?php endif; ?>
--- views/jelly/field/manytomany.php
+++ views/jelly/field/manytomany.php
@@ -1,4 +1,4 @@
-<select name="<?php echo $name ?>[]" id="field-<?php echo $name ?>" multiple>
+<select name="<?php echo $name ?>[]" id="field-<?php echo $name ?>" multiple="multiple">
 	<?php foreach(Jelly::factory($foreign_model)->load() as $related): ?>
 		<?php if (in_array($related->id(), $ids)): ?>
 			<option value="<?php echo $related->id() ?>" selected='selected'><?php echo $related->name()?></option>
