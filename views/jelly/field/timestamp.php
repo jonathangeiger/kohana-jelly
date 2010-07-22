4d9fe90334749e536239ab8dd9d3ea3a1193f151
Jonathan Geiger <jonathan@jonathan-geiger.com>
2010-07-21 21:43:08 -0400

    Closes #131. Thanks gerasim!

--- views/jelly/field/timestamp.php
+++ views/jelly/field/timestamp.php
@@ -1,1 +1,1 @@
-<?php echo Form::input($name, date($pretty_format, $value), $attributes + array('id' => 'field-'.$name)); ?>
+<?php echo Form::input($name, ( ! isset($default) && ! isset($value)) ? '' : date($pretty_format, $value), $attributes + array('id' => 'field-'.$name));
